756e1412059f6b63cf9c71c7165d2cdf18d6d194
Ryan Wendell <Ryan.wendell@me.com>
2022-11-12 21:23:09 -0800

    Deployment

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, './frontend/build')));
 app.use(bodyParser.json())
 app.use(defineCurrentUser)
 
